8e405a4fc0d4104db7ec8a619d6abfce290f083b
bissagouem-eng <bissagouem@gmail.com>
2025-11-18 19:03:24 +0000

    Create app.py

---
+++ app.py
@@ -0,0 +1,5 @@
+self.PART_URLS = [
+    "https://drive.google.com/uc?id=YOUR_ACTUAL_FILE_ID_1&export=download",
+    "https://drive.google.com/uc?id=YOUR_ACTUAL_FILE_ID_2&export=download",
+    "https://drive.google.com/uc?id=YOUR_ACTUAL_FILE_ID_3&export=download",
+]
